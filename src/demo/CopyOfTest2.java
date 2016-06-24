e9c874daf430e8ac4f3f043b48689583885198a2
cqzhouzhonglin <apksoft_123>
2016-06-24 11:41:01 +0800

    copr 偶发  test2

---
+++ src/demo/CopyOfTest2.java
@@ -0,0 +1,35 @@
+package demo;
+
+public class CopyOfTest2 {
+    public static void main(String[] args) {
+        System.err.println();
+        System.err.println();
+        System.err.println();
+        System.err.println();
+        System.err.println();
+        System.err.println();
+        System.err.println();
+        System.err.println();
+        System.err.println();
+        System.err.println();
+        System.err.println();
+        System.err.println();
+        System.err.println();
+        System.err.println();
+        
+        System.err.println();
+        System.err.println();
+        System.err.println();
+        System.err.println();
+        System.err.println();
+        System.err.println();
+        System.err.println();
+        System.err.println();
+        System.err.println();
+        System.err.println();
+        System.err.println();
+        System.err.println();
+        System.err.println();
+        System.err.println();
+    }
+}
